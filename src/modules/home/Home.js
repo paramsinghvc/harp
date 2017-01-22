0a2058d919ee7d8059d7ae2c637f828079576404
paramsingh88 <parminder@Parminders-MacBook-Pro.local>
2017-01-22 20:33:52 +0530

    Lazy Loading

---
+++ src/modules/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+require('./Home.scss');
+
+export default class Home extends Component {
+    render() {
+        return (
+        	<div className="home">
+        	<p>Home Works!</p>
+        	</div>
+        )
+    }
+}
